03aedf356370c21f60ae45a7889800b64fe4da7d
Jennifer Agbor <solacelorraine@gmail.com>
2022-06-02 15:58:41 -0700

    task-4

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multili
+ngual puzzle");
+	return(0);
+}
