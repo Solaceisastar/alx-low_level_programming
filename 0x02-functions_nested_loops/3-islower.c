b115e52344024f328bcd4552be40608215ababc0
Jennifer Agbor <solacelorraine@gmail.com>
2022-06-08 17:34:45 -0700

    lowers

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,9 @@
 
 /**
  * _islower - checks for lowercase character
- * @c: the character to check
+ * @c: contains the value to be compared
  * Return: 1 if c is lowercase, 0 otherwise
- */ 
+ */
 int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
