73618b48d38b5a6c523ff91182238e2768a586e4
Jennifer Agbor <solacelorraine@gmail.com>
2022-06-22 09:57:51 -0700

    task_1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _print_rev_recursion - print a string in reverse
+ * _print_rev_recursion - prints a string in reverse
  * @s: string
- * Return: Always (0)
+ * Return: Always 0
  */
-
 void _print_rev_recursion(char *s)
 {
 	if (*s)
