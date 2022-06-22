4aa4f4cfa4cfbf4898c09f64f11946ea61ac898c
Jennifer Agbor <solacelorraine@gmail.com>
2022-06-22 09:19:33 -0700

    task_0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - print a string, followed by a new line.
+ * @s : string
+ * Return : Always (0)
+ */
+void _puts_recursion(char *s);
+{
+	puts(s);
+}
