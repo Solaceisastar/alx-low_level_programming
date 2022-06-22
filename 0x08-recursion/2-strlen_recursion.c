703b2b63d44da557fe6ea658acd3666a1909e08f
Jennifer Agbor <solacelorraine@gmail.com>
2022-06-22 10:27:43 -0700

    task_2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ * Return: return the length of a string 
+ */
+int _strlen_recursion(char *s)
+{
+	int n;
+	n = strlen(s);
+
+	return (0);
+}
