a045dd7be0dbc8e0e1ce198afbf0d5f3f7f4b41a
Jennifer Agbor <solacelorraine@gmail.com>
2022-06-22 09:42:32 -0700

    task_1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
+ * Return: Always (0)
+ */
+void _print_rev_recursion(char *s)
+{
+	_print_rev(s);
+}
