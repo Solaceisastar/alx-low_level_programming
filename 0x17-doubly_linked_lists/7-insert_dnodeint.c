a7e3a7b81c7bb7c7ad132ee023d3169a6f5d5f48
Jennifer Agbor <solacelorraine@gmail.com>
2022-09-02 19:45:03 -0700

    soul

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,72 +1,52 @@
-#include <stdlib.h>
-
 #include "lists.h"
-
-
 
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: a pointer to the starting node
- * @idx: the index where the new node is to be inserted
- * @n: the data to be inserted in that node
- *
- * Return: the address of the new node, else NULL
+ * @h: pointer to head node
+ * @idx: index of nodes
+ * @n: elements of nodes
+ * Return: address of new node, or NULL if failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-
 {
-
-	dlistint_t *tmp = *h, *new;
-
-
-
-	if (idx == 0)
-
-		return (add_dnodeint(h, n));
-
-
-
-	for (; idx != 1; idx--)
-
-	{
-
-		tmp = tmp->next;
-
-		if (tmp == NULL)
-
-			return (NULL);
-
-	}
-
-
-
-	if (tmp->next == NULL)
-
-		return (add_dnodeint_end(h, n));
-
-
+	unsigned int count = 1;
+	dlistint_t *temp = NULL, *new = NULL;
 
 	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
-
+	if (new == NULL || h == NULL)
 		return (NULL);
-
-
-
 	new->n = n;
-
-	new->prev = tmp;
-
-	new->next = tmp->next;
-
-	tmp->next->prev = new;
-
-	tmp->next = new;
-
-
-
+	temp = *h;
+	if (idx == 0)
+	{
+		*h = new;
+		new->next = temp;
+		new->prev = NULL;
+		temp->prev = new;
+		return (new);
+	}
+	while (temp->next != NULL)
+	{
+		if (count == idx)
+		{
+			new->prev = temp;
+			new->next = temp->next;
+			temp->next = new;
+			new->next->prev = new;
+		}
+		temp = temp->next;
+		count++;
+	}
+	if (count == idx)
+	{
+		new->prev = temp;
+		new->next = NULL;
+		temp->next = new;
+	}
+	if (count < idx)
+	{
+		free(new);
+		return (NULL);
+	}
 	return (new);
-
 }
